1c317cfa4f3a3deac189f288be990e7b45b57304
tinachen <https://gitlab.silkrode.com.tw/silkrode/atw_project_web.git>
2019-12-26 13:03:39 +0800

    Initial commit

--- crud-test/src/components/HelloWorld.vue
+++ crud-test/src/components/HelloWorld.vue
@@ -1,27 +1,80 @@
 <template>
-  <div class="hello">
+  <div class="crudlist">
     <h1>飲料訂單列表</h1>
-    <button @click="addlist">新增訂單</button>
+    <form id="app"
+      @submit="checkForm"
+      action="/something"
+      method="post">
+      <div>
+        <label for="additemName">{{additemName}}</label>
+        <input type="text"
+          name="additemName"
+          id="additemName"
+          min="0">
+        <!-- 2 -->
+        <label for="additemNumber">{{additemNumber}}</label>
+        <input type="text"
+          name="additemNumber"
+          id="additemNumber"
+          min="0">
+        <!-- 3 -->
+        <label for="additemUser">{{additemUser}}</label>
+        <input type="text"
+          name="additemUser"
+          id="additemUser"
+          min="0">
+        <!-- 4 -->
+        <label for="additemOther">{{additemOther}}</label>
+        <input type="text"
+          name="additemOther"
+          id="additemOther"
+          min="0">
+        <button @click="addlist">新增訂單</button>
+      </div>
+
+    </form>
+
     <br>
-    <li>
-      <ul v-for="item in list"
+    <hr>
+    <table>
+      <!-- title -->
+      <tr>
+        <th v-for="header in tableTitle"
+          :key="header.title">{{header.title}}</th>
+      </tr>
+
+      <!-- content -->
+      <tr v-for="item in list"
         :key="item._id">
-        {{item.name}}
+        <td>{{item.name}}</td>
+        <td>{{item.price}}</td>
+        <td>{{item.username}}</td>
+        <td>{{item.description}}</td>
         <button @click="editlist">編輯</button>
         <button @click="deletelist">刪除</button>
-      </ul>
-    </li>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: "hello",
+  name: "crudlist",
+  apiurl:
+    "https://restfull-api-todolist.herokuapp.com/api/v1/orders?limit=3&page=1",
   props: {
     msg: String
   },
+
   data() {
     return {
+      results: [],
+      additemName: "飲料名稱",
+      additemNumber: "價格",
+      additemUser: "訂購人姓名",
+      additemOther: "備註",
+      checkForm: "",
       list: [
         {
           createdAt: "2019-12-24T08:38:51.412Z",
@@ -47,8 +100,45 @@ export default {
           username: "jessica",
           description: "少冰全糖"
         }
+      ],
+      tableTitle: [
+        {
+          title: "飲料名稱"
+        },
+        {
+          title: "價格"
+        },
+        {
+          title: "訂購人姓名"
+        },
+        {
+          title: "備註"
+        }
       ]
     };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://restfull-api-todolist.herokuapp.com/api/v1/orders?limit=3&page=1"
+      )
+      .then(response => {
+        this.results = response.data.results;
+      });
+  },
+  methods: {
+    addlist() {
+      var days = new Date();
+      this.list.push({
+        createdAt: days
+        // name: this.additem,
+        // price: this.addprice,
+        // username: this.username,
+        // description: this.description
+      });
+    },
+    editlist() {},
+    deletelist() {}
   }
 };
 </script>
